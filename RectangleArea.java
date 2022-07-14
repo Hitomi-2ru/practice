dcd88c58032a722f3209e8d8c7d2fad6a2d52037
Hitomi-2ru <109119684+Hitomi-2ru@users.noreply.github.com>
2022-07-14 14:23:16 +0900

    Create RectangleArea.java

---
+++ RectangleArea.java
@@ -0,0 +1,9 @@
+public class RectangleArea {
+	public static void main(String[] args) {
+		int a=3;
+		int b=5;
+		int c=a*b;
+		System.out.println("縦幅３横幅５の長方形の面積は、１５");
+		System.out.println("縦幅"+a+"横幅"+b+"の長方形の面積は、"+c);
+	}
+}
